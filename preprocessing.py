96fcb073509499f175d4b4da67f53c85ba594f5c
1064064 <boyangs@student.unimelb.edu.au>
2019-09-08 14:19:52 +1000

    Split dictionary

--- preprocessing.py
+++ preprocessing.py
@@ -1,2 +1,23 @@
 import numpy as np
+import os
+import string
+
+
+def splitdictionary(capital):
+    with open('dict.txt','r') as dictionary:
+        data = dictionary.readlines()
+        i = 0
+        output = ""
+        for line in data:
+            if not len(line) == 0:
+                if line[0] == capital:
+                    output = output + line
+    file = open('smallDictionary/dict'+capital+'.txt','w')
+    file.write(str(output))
+
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+for char in alphabet:
+    splitdictionary(char)
+
 
