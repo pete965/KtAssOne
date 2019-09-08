d5584ac85d0cc5f2f0153ff1a43dc7e99a124cba
1064064 <boyangs@student.unimelb.edu.au>
2019-09-08 15:42:38 +1000

    取得公共前缀长度dict

--- preprocessing.py
+++ preprocessing.py
@@ -16,8 +16,48 @@ def splitdictionary(capital):
     file.write(str(output))
 
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-for char in alphabet:
-    splitdictionary(char)
+with open('blends.txt','r') as blends:
+    data = blends.readlines()
+    max = 0
+    dist = dict()
+    for i in range(10):
+        dist[i] = 0
+    for line in data:
+        # print(line)
+        out,in1,in2 = line.split("\t")
+        stop = False
+        i = 0
+        local = 0
+        localrev = 0
+        for i in range(min(len(out),len(in1))):
+            if out[i] == in1[i]:
+                local = local + 1
+            else:
+                break
+        i = 0
+        for i in range(min(len(out),len(in2))):
+            if out[i] == in2[i]:
+                localrev = localrev + 1
+            else:
+                break
+        if local < localrev:
+            local = localrev
+        dist[local] = dist[local] + 1
+        if local == 0:
+            print("#################")
+            print(out)
+            print(in1)
+            print(in2)
+            print("#################")
+        if local > max:
+            max = local
+            print(out)
+            print(in1)
+            print(in2)
+        local = 0
+    print(max)
+    print(dist)
 
-
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# for char in alphabet:
+#     splitdictionary(char)
